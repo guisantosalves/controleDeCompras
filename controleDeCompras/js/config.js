c1acb41c7bd2c2044738a616b6cdd1e680b14c74
guisantosalves <59509356+guisantosalves@users.noreply.github.com>
2021-12-04 02:15:42 -0400

    Projeto completo

---
+++ controleDeCompras/js/config.js
@@ -0,0 +1,10 @@
+function setConfig(){
+    var texts = {
+        "title": "Shopping controll" 
+    };
+
+    document.title = texts.title;
+    document.getElementById("navTittle").innerHTML = texts.title;
+}
+
+setConfig();
